79ccc2171024971b8b777d0899f79588dc804b01
Knight <121831468+knight-gangsta@users.noreply.github.com>
2023-11-19 22:30:12 -0500

    Products

---
+++ src/views/Paymode.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">    
+    <h1>Pay Mode List</h1>    
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Observation</th>           
+        </tr>
+    </thead>
+    <tbody>       
+        <tr>
+            <th v-for="(paymode, index)in paymodes" :key="index">
+            <th scope="row">{{ index +1 }}</th>
+            <td>{{ paymode.id }}</td>
+            <td>{{ paymode.name }}</td>
+            <td>{{ paymode.observation }}</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+    name: 'Paymode',
+    data(){
+        return{
+            paymodes: []
+        }
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:8000/api/paymodes')
+            .then(response => (this.paymodes = response.data.paymodes))
+    },
+}
+</script>
---
+++ src/views/Products.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container">    
+    <h1>Products List</h1>    
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th> 
+            <th scope="col">Stock</th> 
+            <th scope="col">Category Id</th>           
+        </tr>
+    </thead>
+    <tbody>       
+        <tr>
+            <th v-for="(product, index)in products" :key="index">
+            <th scope="row">{{ index +1 }}</th>
+            <td>{{ product.id }}</td>
+            <td>{{ product.name }}</td>
+            <td>{{ product.price }}</td>
+            <td>{{ product.stock }}</td>
+            <td>{{ product.categoryid }}</td>
+
+
+        </tr>
+        </tbody>
+    </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+    name: 'Product',
+    data(){
+        return{
+            products: []
+        }
+    },
+    mounted() {
+        axios
+            .get('http://127.0.0.1:8000/api/products')
+            .then(response => (this.products = response.data.products))
+    },
+}
+</script>
